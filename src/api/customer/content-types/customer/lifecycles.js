d008ac563beb6136db3d39ef68d2a3d840de6837
bramv-d <bramjvandriel@gmail.com>
2023-02-16 16:14:07 +0100

    Implemented sendgrid

---
+++ src/api/customer/content-types/customer/lifecycles.js
@@ -0,0 +1,35 @@
+module.exports = {
+    async afterCreate(event) {
+        const {result} = event;
+        try {
+            await strapi.plugins['email'].services.email.send(
+                {
+                    "from":{
+                        "email":"info@logue.audio"
+                    },
+                    "personalizations":[
+                        {
+                            "to":[
+                                {
+                                    "email": result.email
+                                }
+                            ],
+                            "dynamic_template_data":{
+                                "name": result.name,
+                                "address": result.address,
+                                "postalCode": result.postalCode,
+                                "city": result.city,
+                                "phoneNumber": result.phoneNumber,
+                                "subscribed": result.subscribed
+                            }
+                        }
+                    ],
+                    "template_id":"d-353bf9aaa94746a7baefa573574ef171"
+                }
+            )
+        } catch (err){
+            console.log(err);
+        }
+    }
+}
+
